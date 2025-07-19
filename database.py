97805f4ca53569620891040ec4dcb3813b9b3e81
Sumit Ranjan <98314060+ranjanxsumit@users.noreply.github.com>
2025-07-19 19:36:22 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio
 from config import settings
 
-client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongo_uri)
+client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URI)
 db = client.get_database("ecommerce")
 product_collection = db.get_collection("products")
 order_collection = db.get_collection("orders")
