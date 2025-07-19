449127007f1f298c23f504fd4bf55080a2bb3cc7
Sumit Ranjan <98314060+ranjanxsumit@users.noreply.github.com>
2025-07-19 18:07:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ app.include_router(orders.router, tags=["Orders"])
 @app.get("/", tags=["Root"])
 def read_root():
     return {"message": "Welcome to the E-Commerce API"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
